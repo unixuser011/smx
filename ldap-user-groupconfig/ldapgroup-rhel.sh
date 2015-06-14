db761237f0bd45ad5ff6bfdb074a3930d17721eb
Darius Anderson <d.anderson1147@gmail.com>
2015-06-14 14:29:33 +0100

    Adding ldap group config script for rhel

---
+++ ldap-user-groupconfig/ldapgroup-rhel.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+##
+
+read -p "Enter domain name > " domainName
+read -p "Enter top level domain name > " tldName
+
+SUFFIX='dc=$domainName,dc=$tldName'
+LDIF='ldapuser.ldif'
+
+for line in `grep "x:[5-9][0-9][0-9]:" /etc/group`
+do
+    CN="`echo $line | cut -d: -f1`"
+    LGID="`echo $line | cut -d: -f3`"
+    
+    echo "dn: cn=$CN,ou=Group,$SUFFIX" >> $LDIF
+    echo "objectClass: posixGroup" >> $LDIF
+    echo "cn: $CN" >> $LDIF
+    echo "gidNumber: $LGID" >> $LDIF
+    echo "memberUid: `grep ":$LGID:" /etc/passwd | cut -d: -f1`" >> $LDIF
+
+    users="`echo $line | cut -d: -f4`"
+    if [ "$users" ]
+    then
+        for user in `echo "$users" | sed 's/,/ /g'`
+        do
+            [ ! "$CN" = "$user" ] && echo "memberUid: $user" >> $LDIF
+        done
+    fi
+    echo >> $LDIF
+done
